b5fd2f041adede7fb604785d57da7a1a1eae392b
yangzhan <yzlz5259@163.com>
2020-09-03 21:47:05 +0800

    引入 模板引擎 koa-nunjucks-2

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const controller = require('./controller');
 const app = new Koa();
+const path = require('path');
+const koaNunjucks = require('koa-nunjucks-2');
 
 // 注册 bodyParser，必须在 router之前被注册
-app.use(bodyParser());
+// app.use(bodyParser());
+
+// 使用 koa-nunjucks-2 实例获得中间件
+app.use(
+	koaNunjucks({
+		ext: 'html', // 使用HTML后缀的模板
+		path: path.join(__dirname, 'views'), // 模板所在路径
+		nunjucksConfig: {
+			// nunjucks的配置
+			trimBlocks: true,
+		},
+	}),
+);
 
 app.use(async (ctx, next) => {
 	// console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
@@ -12,7 +26,7 @@ app.use(async (ctx, next) => {
 	if (ctx.status == 404) {
 		// 错误处理的中间件
 		ctx.status = 404;
-		ctx.body = `<h3>404 页面</h3>`;
+		await ctx.render('404');
 	}
 });
 
