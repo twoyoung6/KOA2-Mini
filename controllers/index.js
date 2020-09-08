9aa83301b83c070101a809602a81a11935de6d7a
yangzhan <yzlz5259@163.com>
2020-09-08 23:01:07 +0800

    cookie 使用

--- controllers/index.js
+++ controllers/index.js
@@ -16,6 +16,16 @@ var fn_doSign = async (ctx, next) => {
         value: ctx.request.body.password,
       },
     ]
+    // cookie 中间件
+    ctx.cookies.set('name', ctx.request.body.name, {})
+    ctx.cookies.set('password', ctx.request.body.password, {
+      path: '',
+      domain: '',
+      expires: '', // max-age
+      httpOnly: true,
+      secure: '',
+    })
+
     await ctx.render('dosign', { items: arr })
   } else {
     ctx.response.body = `<h1>登录失败，请返回重新登录！</h1>
